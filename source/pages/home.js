ab9782e9a77c87e6b3b563c17a0aea1918d9bb50
Daniel Sokil <dtechtours@gmail.com>
2020-05-19 18:25:55 -0400

    Refactor Project To Microverse Students Directory

--- source/pages/home.js
+++ source/pages/home.js
@@ -1,36 +1,16 @@
-(() => {
-  const Kenya_Popular_Schools = [
-    "Banda School",
-    "Braeburn School Nairobi",
-    "Greensteds International School",
-    "Hillcrest International Schools",
-    "International School of Kenya",
-    "Kenton College Preparatory School",
-    "Pembroke House",
-    "Peponi House Preparatory School",
-    "Pangani Girls High School",
-    "Alliance Girls",
-    "Mangu High",
-    "Alliance High",
-    "Maseno School",
-    "Kapsabet Boys",
-    "Limuru Girls",
-    "Moi Girls Eldoret",
-    "Lenana School",
-    "MaryHill High School",
-    "Nairobi School",
-    "Nakuru Girls",
-    "Kenya High",
-    "Starehe Boys",
-    "Maranda High",
-  ];
+(async () => {
+  const STUDENTS = await fetch("./data/students-formatted.json").then((r) =>
+    r.json()
+  );
+
+  const studentNames = STUDENTS.map((item) => item["student"]);
 
   window.loadjs(
     "https://cdn.jsdelivr.net/gh/s0kil/inputyper@6e03fc2fe660bf1c75fc70eb320c0f6dd77ed61e/index.js",
     () => {
       PlaceholderTypingEffect({
         element: document.getElementById("school-search"),
-        strings: Kenya_Popular_Schools,
+        strings: studentNames,
         prepend: 'e.g. "',
         append: '"',
       }).begin({ delay: 1000 });
